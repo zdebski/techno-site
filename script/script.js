fbe806dd2c2c55745675b7473d50d99525b35ff1
DESKTOP-OOCO1QK\PRZEMEK <ajirebardyn21@gmail.com>
2019-04-29 11:37:23 +0200

    Home page

---
+++ script/script.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function(){
+    
+    
+
+
+var navButton = document.querySelector(".navigation-button");
+var navMenu = document.querySelector(".navigation-menu");
+var win = window;
+
+function openMenu(event) {
+    console.log("Dupa");
+  navButton.classList.toggle("active");
+  navMenu.classList.toggle("active");
+
+  event.preventDefault();
+  event.stopImmediatePropagation();
+}
+
+
+function closeMenu(event) {
+  if (navButton.classList.contains("active")) {
+    navButton.classList.remove("active");
+    navMenu.classList.remove("active");
+  }
+}
+
+navButton.addEventListener("click", openMenu, false);
+
+
+win.addEventListener("click", closeMenu, false);
+    
+})
+
